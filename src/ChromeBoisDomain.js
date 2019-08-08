2015162233b9fbde8f1df9d2cb1ad3d7ba0cbe83
github username <github email address>
2019-08-08 16:41:30 -0700

    Done.

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -1,32 +1,33 @@
-import React, { Component } from 'react';
-import { drawChromeBoiAtCoords, toggleCycling, resize } from './canvasHelpers.js'
-
+import React, { Component } from "react";
+import {
+  drawChromeBoiAtCoords,
+  toggleCycling,
+  resize
+} from "./canvasHelpers.js";
 
 export default class ChromeBoisDomain extends Component {
-
-  handleMouseMove = (event) => {
-    drawChromeBoiAtCoords(event.clientX, event.clientY)
-  }
+  handleMouseMove = event => {
+    drawChromeBoiAtCoords(event.clientX, event.clientY);
+  };
 
   handleDownKey = (event) => {
-    if (event.key === 'a') {
-      resize('+')
-    } else if (event.key === 's') {
-       resize('-')
+    if (event.key === "a") {
+      resize("+");
+    } else if (event.key === "s") {
+      resize("-");
     }
-  }
+  };
 
   render() {
     return (
       <canvas
         onMouseMove={this.handleMouseMove}
-        width='900'
-        height='600'
-        tabIndex="0">
-
-        onClick={()=> {this.toggleCycling()}}
-        onKeyDown={this.handleDownKey}
-      </canvas>
-    )
+        width="900"
+        height="600"
+        tabIndex="0"
+        onClick={() => toggleCycling()}
+        onKeyPress={this.handleDownKey}
+      ></canvas>
+    );
   }
 }
